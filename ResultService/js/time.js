c147c729f54ea4b8ddb810833edb1f892417dd78
Jarno Matarmaa <jarno.matarmaa@tuni.fi>
2021-12-05 00:42:51 +0500

    Responsive update
    Home page responsivity settings

--- ResultService/js/time.js
+++ ResultService/js/time.js
@@ -1,5 +1,26 @@
 // request a weekday along with a long date
-var options = {
+function compnavitems() {
+    var x = document.getElementById("respnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+let options1 = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC',
+    timeZoneName: 'short'
+};
+
+let options2 = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
     weekday: 'long',
     year: 'numeric',
     month: 'long',
@@ -8,10 +29,7 @@ var options = {
     timeZoneName: 'short'
 };
 
-
-
 setInterval(function() {
-    var date = new Date();
-    date.toLocaleString('fi-FI', options);
-    document.getElementById("date").innerHTML = date;
+    var date = new Date(); // → "Donnerstag, 20. Dezember 2012"
+    document.getElementById("date").innerHTML = date.toLocaleString('de-DE', options2);
 }, 1000);
